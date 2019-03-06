3d372180c8871307a077739713ee70a6159012a9
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2019-03-06 08:38:44 -0500

    Add files via upload

---
+++ src/DriverExam.java
@@ -0,0 +1,14 @@
+
+public class DriverExam {
+	private char[] correctAnswers;
+	
+	public  DriverExam(char[] c)
+	{
+		correctAnswers = c;
+	}
+	public double totalCorrect(char[] studentAnswers)
+	{
+		for(int i = 0; i < student)
+	}
+	
+}
---
+++ src/DriverExamTest.java
@@ -0,0 +1,12 @@
+
+public class DriverExamTest {
+
+	public static void main(String[] args) {
+		char[] answers = {};
+		DriverExam test = new DriverExam(answers);
+		// create a student's driver exam object
+		//check to see totalCorrect by running method
+
+	}
+
+}
